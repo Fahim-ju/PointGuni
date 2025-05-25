a5b3b8570e00d7c9cea50329a051edc8ab47afb2
Md Fahimul Karim <f.nahian1997@gmail.com>
2025-05-26 00:48:22 +0600

    Fixed List View

--- src/component/ListPointView.tsx
+++ src/component/ListPointView.tsx
@@ -1,8 +1,10 @@
-import { View, Text, StyleSheet, ScrollView } from "react-native";
+import { View, Text, StyleSheet, ScrollView, Dimensions } from "react-native";
 import React from "react";
 import { Player } from "../types/Player";
 import { useGameContext } from "../context/GameContext";
 import { Round } from "../types/Game";
+
+const screenHeight = Dimensions.get("window").height;
 
 const ListPointView = () => {
   const { players } = useGameContext();
@@ -22,38 +24,43 @@ const ListPointView = () => {
     return tempRounds;
   }, [players]);
 
+  console.log("screenheight", screenHeight);
+
   return (
-    <ScrollView horizontal contentContainerStyle={styles.scrollContainer}>
+    <ScrollView horizontal contentContainerStyle={styles.scrollContainer} showsHorizontalScrollIndicator={false}>
       <View style={styles.tableWrapper}>
-        <View style={styles.tableRow}>
-          <Text style={[styles.tableCell, styles.tableHeader, styles.firstCell]}>SL.</Text>
-          <Text style={[styles.tableCell, styles.tableHeader]}>Hannan</Text>
-          <Text style={[styles.tableCell, styles.tableHeader]}>Fahimul</Text>
-          <Text style={[styles.tableCell, styles.tableHeader]}>Nahina</Text>
-          <Text style={[styles.tableCell, styles.tableHeader]}>RAhima</Text>
+        <View style={[styles.tableRow, { backgroundColor: "#F5F1FA" }]}>
+          <Text style={[styles.tableCell, styles.tableHeader, styles.firstCell, { color: "#6C3FC5" }]}>T</Text>
+          <Text style={[styles.tableCell, styles.tableHeader, { color: "#6C3FC5" }]}>{players?.[0]?.totalPoints ?? "_"}</Text>
+          <Text style={[styles.tableCell, styles.tableHeader, { color: "#6C3FC5" }]}>{players?.[1]?.totalPoints ?? "_"}</Text>
+          <Text style={[styles.tableCell, styles.tableHeader, { color: "#6C3FC5" }]}>{players?.[2]?.totalPoints ?? "_"}</Text>
+          <Text style={[styles.tableCell, styles.tableHeader, { color: "#6C3FC5" }]}>{players?.[3]?.totalPoints ?? "_"}</Text>
         </View>
-        {rounds.map((round, index) => (
-          <View
-            key={index}
-            style={[
-              styles.tableRow,
-              { backgroundColor: index % 2 === 0 ? "#F3E9FC" : "#fff" }, 
-            ]}
-          >
-            <Text style={[styles.tableCell, styles.tableData, styles.firstCell]}>{index + 1}</Text>
-            <Text style={[styles.tableCell, styles.tableData]}>{round.scores?.[0].points ?? "-"}</Text>
-            <Text style={[styles.tableCell, styles.tableData]}>{round.scores?.[1].points ?? "-"}</Text>
-            <Text style={[styles.tableCell, styles.tableData]}>{round.scores?.[2].points ?? "-"}</Text>
-            <Text style={[styles.tableCell, styles.tableData]}>{round.scores?.[3].points ?? "-"}</Text>
-          </View>
-        ))}
+        <View style={[styles.tableRow, { backgroundColor: "#6C3FC5" }]}>
+          <Text style={[styles.tableCell, styles.tableHeader, styles.firstCell, { color: "#fff" }]}>SL.</Text>
+          <Text style={[styles.tableCell, styles.tableHeader, { color: "#fff" }]}>{players?.[0].name ?? "-"}</Text>
+          <Text style={[styles.tableCell, styles.tableHeader, { color: "#fff" }]}>{players?.[1].name ?? "-"}</Text>
+          <Text style={[styles.tableCell, styles.tableHeader, { color: "#fff" }]}>{players?.[2].name ?? "-"}</Text>
+          <Text style={[styles.tableCell, styles.tableHeader, { color: "#fff" }]}>{players?.[3].name ?? "-"}</Text>
+        </View>
+        {/* Scrollable Body */}
+        <ScrollView style={[styles.bodyScroll]} showsVerticalScrollIndicator={false}>
+          {rounds.map((round, index) => (
+            <View key={index} style={[styles.tableRow, { backgroundColor: index % 2 === 0 ? "#F3E9FC" : "#fff" }]}>
+              <Text style={[styles.tableCell, styles.tableData, styles.firstCell]}>{index + 1}</Text>
+              <Text style={[styles.tableCell, styles.tableData]}>{round.scores?.[0].points ?? "-"}</Text>
+              <Text style={[styles.tableCell, styles.tableData]}>{round.scores?.[1].points ?? "-"}</Text>
+              <Text style={[styles.tableCell, styles.tableData]}>{round.scores?.[2].points ?? "-"}</Text>
+              <Text style={[styles.tableCell, styles.tableData]}>{round.scores?.[3].points ?? "-"}</Text>
+            </View>
+          ))}
+        </ScrollView>
       </View>
     </ScrollView>
   );
 };
 
 export default ListPointView;
-
 const styles = StyleSheet.create({
   scrollContainer: {
     padding: 10,
@@ -68,7 +75,7 @@ const styles = StyleSheet.create({
     borderRadius: 5,
   },
   tableCell: {
-    padding: 8,
+    padding: 10,
     alignItems: "center",
     textAlign: "center",
     width: 80,
@@ -76,15 +83,25 @@ const styles = StyleSheet.create({
     maxHeight: 40,
   },
   firstCell: {
+    fontWeight: "500",
     width: 40,
   },
   tableHeader: {
     fontSize: 16,
     fontWeight: "bold",
-    backgroundColor: "#6C3FC5",
-    color: "#fff", 
+    color: "#fff",
+  },
+  tableSumHeader: {
+    fontSize: 16,
+    fontWeight: "bold",
+    backgroundColor: "gray",
+    color: "#fff",
   },
   tableData: {
-    fontSize: 16,
+    fontSize: 18,
+    fontWeight: "600",
+  },
+  bodyScroll: {
+    maxHeight: screenHeight,
   },
 });
